cfaab5926b353ec2c64422d7b6fb0fa86a4505e7
azhar416 <rar.azhar.416@gmail.com>
2021-03-25 15:37:55 +0700

    update nomor 1

--- soal1/soal1.sh
+++ soal1/soal1.sh
@@ -5,6 +5,7 @@ IFS=
 # regex="(INFO |ERROR )(.*)((?=[\(])(.*))"
 regex1="(?<=ERROR )(.*)(?=\ )"
 regex2="(?<=[(])(.*)(?=[)])"
+regex3="(?=[(])(.*)(?<=[)])"
 input="syslog.log";
 # str="Jan 31 00:16:25 ubuntu.local ticky: INFO Closed ticket [#1754] (noel)"
 
@@ -16,7 +17,7 @@ error_msg=$(grep -oP "$regex1" "$input" | sort)
 # 1c
 error=$(grep -oP "ERROR.*" "$input")
 # echo $error
-# grep -oP "$regex2" <<< "$error" | sort | uniq -c
+# grep -oP "$regex2" <<< "$error" | sort | uniq
 info=$(grep -oP "INFO.*" "$input")
 # echo $info
 # grep -oP "$regex2" <<< "$info" | sort | uniq -c
@@ -31,9 +32,17 @@ printf "%s,%d\n" "$em" "$count" >> "error_message.csv"
 done 
 
 # 1e
-
-
-
+printf "Username,INFO,ERROR\n" > "user_statistic.csv"
+grep -oP "$regex3" <<< "$error" | sort | uniq | while read -r er; do
+user=$(grep -oP "(?<=[(])(.*)(?=[)])" <<< "$er")
+n_error=$(grep "$er" <<< "$error" | wc -l)
+n_info=$(grep "$er" <<< "$info" | wc -l)
+# echo $er
+# echo $user
+# echo $n_error
+# echo $n_info
+printf "%s,%d,%d\n" "$user" "$n_info" "$n_error" >> "user_statistic.csv"
+done
 
 
 

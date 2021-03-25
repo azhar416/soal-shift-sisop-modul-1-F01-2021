0271844550da388547e826f6600d329884911031
Inez Yulia Amanda <inez.amanda16@gmail.com>
2021-03-25 16:41:33 +0800

    tes output

--- soal2/soal2.sh
+++ soal2/soal2.sh
@@ -1,8 +1,9 @@
-#!/bin/zsh
+#!/bin/bash
 
 export LC_ALL=C
-awk -F "\t" 'BEGIN {} { 
-    profitPercentage=$21/($18-$21)*100} {
+awk 'BEGIN {FS="\t"} { 
+    max = 0
+    profitPercentage=($21 / ($18-$21)) * 100} {
         if (max<=profitPercentage){
             max=profitPercentage
             RowID=$1
@@ -10,11 +11,10 @@ awk -F "\t" 'BEGIN {} {
     }
     END{
         print "Profit percentage terbesar : ", RowID, "(", max, "%)\n"
-    }' /Users/inez_amanda/sisop/p1/Laporan-TokoShiSop.tsv >> result.txt
+    }' /Users/inez_amanda/sisop/p1/soal-shift-sisop-modul-1-F01-2021/soal2/Laporan-TokoShiSop.tsv > result.txt
 
-export LC_ALL=;
-awk -F "\t" 'BEGIN {} { 
-        if ($10 -eq "Albuquerque" && $2~"2017"){
+awk 'BEGIN {FS="\t"} { 
+        if ($10=="Albuquerque" && $2~"2017"){
             customer[$7]+=1
         }
     }
@@ -23,7 +23,7 @@ awk -F "\t" 'BEGIN {} {
         {for(name in customer){
             print name
         }}
-    }' /Users/inez_amanda/sisop/p1/Laporan-TokoShiSop.tsv >> result.txt
+    }' /Users/inez_amanda/sisop/p1/soal-shift-sisop-modul-1-F01-2021/soal2/Laporan-TokoShiSop.tsv > result.txt
 
 # awk -F "\t" 'BEGIN {} {
 #         if(NR>1){ segment[$8]+=1 }

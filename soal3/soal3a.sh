9697fd5bcfffbc3a8d69cba971fd7acad823e83c
rickys20 <73892020+rickys20@users.noreply.github.com>
2021-03-25 12:48:56 -0400

    Update soal3a.sh

--- soal3/soal3a.sh
+++ soal3/soal3a.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
-for i in {1..5}
+declare -A arr
+shopt -s globstar
+
+for i in {1..23}
 do
     wget -O Koleksi_$i https://loremflickr.com/320/240/kitten 
+done
+
+for file in **; do
+  [[ -f "$file" ]] || continue
+   
+  read cksm _ < <(md5sum "$file")
+  if ((arr[$cksm]++)); then 
+    echo "rm $file"
+  fi
 done

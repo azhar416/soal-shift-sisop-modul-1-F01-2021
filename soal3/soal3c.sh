8bd88a7490276423ae999d74e98c2ac5a6dc4aab
rickys20 <73892020+rickys20@users.noreply.github.com>
2021-03-27 03:54:11 -0400

    Update soal3c.sh

--- soal3/soal3c.sh
+++ soal3/soal3c.sh
@@ -1,9 +1,10 @@
 #1/bin/bash
-
-
-
+ 
+n_kucing=$(ls | grep -e "Kucing.*" | wc -l)
+n_kelinci=$(ls | grep -e "Kelinci.*" | wc -l)
+ 
 #kondisi kucing
-if [[  ]] ;
+if [[ $n_kucing -eq $n_kelinci ]] ;
  then 
  i=1
  while [ $i -lt 24 ]
@@ -11,7 +12,7 @@ if [[  ]] ;
     wget -O "Kitten_$i.jpg" -a Foto.log https://loremflickr.com/320/240/kitten
     i=$[$i+1]
  done
-
+ 
  files="$( find -type f )"
  for file1 in $files; do
     for file2 in $files; do
@@ -24,23 +25,24 @@ if [[  ]] ;
         fi
     done
  done
-
+ 
  j=1
  for file in *.jpg; do
     echo $file
     if [[ $j -lt 10 ]]; then
-      mv "$file" "Kucing_0$j.jpg"
+      mv "$file" "Koleksi_0$j.jpg"
     else 
-      mv "$file" "Kucing_$j.jpg"
+      mv "$file" "Koleksi_$j.jpg"
     fi
     j=$[$j+1]
  done
  
  filename=$(date +"%m-%d-%Y")
  mkdir "Kucing_$filename"
- mv ./Kucing_* ./Foto.log "./Kucing_$filename/"
+ mv ./Koleksi_* ./Foto.log "./Kucing_$filename/"
  echo "Berhasil"
-
+ kucing=$[$kucing+1]
+ 
 #kondisi  kelinci 
  else
  i=1
@@ -49,7 +51,7 @@ if [[  ]] ;
     wget -O "Rabbit_$i.jpg" -a Foto.log https://loremflickr.com/320/240/bunny
     i=$[$i+1]
  done
-
+ 
  files="$( find -type f )"
  for file1 in $files; do
     for file2 in $files; do
@@ -62,20 +64,22 @@ if [[  ]] ;
         fi
     done
  done
-
+ 
  j=1
  for file in *.jpg; do
     echo $file
     if [[ $j -lt 10 ]]; then
-      mv "$file" "Kelinci_0$j.jpg"
+      mv "$file" "Koleksi_0$j.jpg"
     else 
-      mv "$file" "Kelinci_$j.jpg"
+      mv "$file" "Koleksi_$j.jpg"
     fi
     j=$[$j+1]
  done
  
  filename=$(date +"%m-%d-%Y")
  mkdir "Kelinci_$filename"
- mv ./Kelinci_* ./Foto.log "./Kelinci_$filename/"
+ mv ./Koleksi_* ./Foto.log "./Kelinci_$filename/"
  echo "Berhasil"
+ kelinci=$[$kelinci+1]
 fi
+ 
